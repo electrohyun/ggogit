a38579a0c573469e9e765e97c586fc7a4dc37c95
electrohyun <dev.electrohyun@gmail.com>
2026-05-20 12:19:19 +0900

    design: (#15) 색상 및 폰트 기초 등록

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,24 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const pretendard = localFont({
+  src: "../assets/fonts/PretendardVariable.woff2",
+  variable: "--font-pretendard",
+  display: "swap",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const schoolSafeDiary = localFont({
+  src: "../assets/fonts/school-safe-diary.otf",
+  variable: "--font-school-safe-diary",
+  display: "swap",
+  weight: "400",
+});
+
+const cascadiaCode = localFont({
+  src: "../assets/fonts/CascadiaCode.woff2",
+  variable: "--font-cascadia-code",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,7 +32,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html
+      lang="ko"
+      className={`${pretendard.variable} ${schoolSafeDiary.variable} ${cascadiaCode.variable}`}
+    >
       <body>{children}</body>
     </html>
   );
