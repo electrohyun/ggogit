7654216b895806f030221b96aedd1b733093851a
electrohyun <dev.electrohyun@gmail.com>
2026-06-04 15:18:27 +0900

    feat: (#12) 앱 셸 사용자 정보 표시 연결

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "k.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "img1.kakaocdn.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
